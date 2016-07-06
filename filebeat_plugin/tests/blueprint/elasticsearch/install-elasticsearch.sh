f05535173cd84a2fa8e7759caa795a1e4c826b27
fogelomer <omer@gigaspaces.com>
2016-07-06 14:08:31 +0300

    poc

--- filebeat_plugin/tests/blueprint/elasticsearch/install-elasticsearch.sh
+++ filebeat_plugin/tests/blueprint/elasticsearch/install-elasticsearch.sh
@@ -40,11 +40,10 @@ function main()
 
     install_elasticsearch &&
 
-    ctx logger info "Uploading Filebeat Index Template to Elasticsearch..."
-    cd /opt/elasticsearch
-    sudo curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json
     ELASTIC=$(ctx instance host_ip)
-    sudo curl -XPUT 'http://${ELASTIC}:9200/_template/filebeat?pretty' -d@filebeat-index-template.json
+#    ctx logger info "Uploading Filebeat Index Template to Elasticsearch..."
+#    cd /opt/elasticsearch
+#    sudo curl -XPUT "http://${ELASTIC}:9200/_template/filebeat?pretty" -d@/etc/filebeat/filebeat.template.json
 
     ctx logger info "Set the ElasticSearch IP runtime property"
     ctx logger info "ElasticSearch IP is ${ELASTIC} "
