670b053db2d85442e881876bf06d4f0dacf54383
fogelomer <omer@gigaspaces.com>
2016-07-07 11:15:01 +0300

    poc

--- filebeat_plugin/tasks.py
+++ filebeat_plugin/tasks.py
@@ -28,6 +28,7 @@
 import os
 import sys
 import shlex
+import tempfile
 import subprocess
 import pkg_resources
 
@@ -41,10 +42,14 @@ from cloudify.decorators import operation
 
 import filebeat_plugin
 
+distro = distro.id()
 
 @operation
-def install(filebeat_config_inputs, filebeat_config_file='',
-            filebeat_install_path='', download_url='', **kwargs):
+def install(filebeat_config_inputs,
+            filebeat_config_file='',
+            filebeat_install_path='',
+            download_url='',
+            **kwargs):
     """Installation operation.
 
     Downloading and installing filebeat packacge - default version is 0.12.0.
@@ -52,12 +57,46 @@ def install(filebeat_config_inputs, filebeat_config_file='',
     Only linux distributions are supported.
     """
     if 'linux' not in sys.platform:
-        raise exceptions.NonRecoverableError('''Error!
-         filebeat-plugin is available on linux distribution only''')
-    dist = distro.id()
+        raise exceptions.NonRecoverableError(
+            'Error! filebeat-plugin is available on linux distribution only')
 
+    installation_file = download_filebeat(filebeat_install_path, download_url)
+    install_filebeat(filebeat_install_path, installation_file)
+    configure(filebeat_config_inputs, filebeat_config_file)
+
+
+@operation
+def start(**kwargs):
+    """Start operation call for filebeat service,
+    with filebeat_plugin configuration file.
+
+    If filebeat service was already running -
+    it will restart it and will use updated configuration file.
+    """
+    ctx.logger.info('Starting filebeat service...')
+    filebeat_config_file = os.path.join('/', 'etc', 'filebeat', 'filebeat.yml')
+    if not os.path.isfile(filebeat_config_file):
+        raise exceptions.NonRecoverableError(
+            "Can't start the service. Wrong config file provided")
+
+    if os.path.exists('/usr/bin/systemctl'):
+        _run('sudo systemctl restart filebeat')
+    else:
+        _run('sudo service filebeat restart')
+
+    ctx.logger.info(
+        'Good Luck! filebeat service is up!'
+        'Have an awesome logging experience...')
+
+
+def download_filebeat(download_url='', filebeat_install_path='', **kwargs):
+    """Downloading filebeat package form your desire url.
+
+    Default url set to be version 0.12.0
+    anf downloaded from official influxdb site.
+    """
     if not filebeat_install_path:
-        filebeat_install_path = '/opt/filebeat'
+        filebeat_install_path = os.path.join('/', 'opt', 'filebeat')
     ctx.instance.runtime_properties[
         'filebeat_install_path'] = filebeat_install_path
     if os.path.isfile(filebeat_install_path):
@@ -67,100 +106,65 @@ def install(filebeat_config_inputs, filebeat_config_file='',
     if not os.path.exists(filebeat_install_path):
         _run('sudo mkdir -p {0}'.format(filebeat_install_path))
 
-    installation_file = download_filebeat(
-        filebeat_install_path, dist, download_url)
-    install_filebeat(filebeat_install_path, dist, installation_file)
-    configure(filebeat_config_inputs, filebeat_config_file)
-
-
-@operation
-def start(filebeat_config_file='', **kwargs):
-    """Start operation call for filebeat service,
-    with filebeat_plugin configuration file.
-
-    If filebeat service was already running -
-    it will restart it and will use updated configuration file.
-    """
-    ctx.logger.info('Starting filebeat service...')
-    if not filebeat_config_file:
-        filebeat_config_file = '/etc/filebeat/filebeat.yml'
-    if not os.path.isfile(filebeat_config_file):
-        raise exceptions.NonRecoverableError("Config file doesn't exists")
-
-    try:
-        _run('sudo /etc/init.d/filebeat restart')
-    except SystemExit:
-        _run('sudo service filebeat restart')
-
-    ctx.logger.info(
-        'GoodLuck! filebeat service is up!'
-        'Have an awesome logging experience...')
-
-
-def download_filebeat(filebeat_install_path, dist, download_url='', **kwargs):
-    """Downloading filebeat package form your desire url.
-
-    Default url set to be version 0.12.0
-    anf downloaded from official influxdb site.
-    """
     ctx.logger.info('Downloading filebeat...')
 
     if not download_url:
-        if dist in ('ubuntu', 'debian'):
+        if distro in ('ubuntu', 'debian'):
             download_url = 'https://download.elastic.co/beats/filebeat/filebeat_1.2.3_amd64.deb'
-        elif dist in ('centos', 'redhat'):
+        elif distro in ('centos', 'redhat'):
             download_url = 'https://download.elastic.co/beats/filebeat/filebeat-1.2.3-x86_64.rpm'
         else:
             raise exceptions.NonRecoverableError(
-                'Error! distribution is not supported')
+                'Error! distribution is not supported. Ubuntu, Debian, Centos and Redhat are supported currently')
     installation_file = _download_file(download_url, filebeat_install_path)
 
-    ctx.logger.info('filebeat downloaded...installing..')
+    ctx.logger.info('filebeat downloaded.')
     return installation_file
 
 
-def install_filebeat(filebeat_install_path, dist, installation_file, **kwargs):
+def install_filebeat(installation_file, filebeat_install_path, **kwargs):
     """Depacking filebeat package."""
     ctx.logger.info('Installing filebeat...')
 
-    if dist in ('ubuntu', 'debian'):
-        cmd = 'sudo dpkg -i {0}/{1}'.format(
-            filebeat_install_path, installation_file)
-    elif dist in ('centos', 'redhat'):
-        cmd = 'sudo rpm -vi {0}/{1}'.format(
-            filebeat_install_path, installation_file)
+    if distro in ('ubuntu', 'debian'):
+        install_cmd = 'sudo dpkg -i {0}'.format(
+            os.path.join(filebeat_install_path, installation_file))
+    elif distro in ('centos', 'redhat'):
+        install_cmd = 'sudo rpm -vi {0}'.format(
+            os.path.join(filebeat_install_path, installation_file))
     else:
         raise exceptions.NonRecoverableError(
-            'Error! distribution is not supported')
-    _run(cmd)
+            'Error! distribution is not supported. Ubuntu, Debian, Centos and Redhat are supported currently')
+    _run(install_cmd)
     ctx.logger.info('filebeat service was installed...')
 
 
-def configure(filebeat_config, filebeat_config_file='', **kwargs):
+def configure(filebeat_config_file='', filebeat_config='' **kwargs):
     """Generating configuration file from your own desire destination
     or from filebeat_plugin filebeat.conf file.
 
     Rendering your inputs/outputs definitions.
     """
-    ctx.logger.info('Configuring filebeat.yml...')
+    ctx.logger.info('Configuring filebeat...')
 
-    if not filebeat_config_file:
+    if filebeat_config_file:
+        ctx.download_resource_and_render(filebeat_config_file,
+                                         template_variables=filebeat_config)
+    else:
         filebeat_config_file_temp = pkg_resources.resource_string(
             filebeat_plugin.__name__, 'resources/filebeat.yml')
         configuration = jinja2.Template(filebeat_config_file_temp)
-        filebeat_config_file = '/tmp/filebeat.conf'
+        filebeat_config_file = tempfile.TemporaryFile()
         with open(filebeat_config_file, 'w') as f:
             f.write(configuration.render(filebeat_config))
-    else:
-        ctx.download_resource_and_render(filebeat_config_file,
-                                         template_variables=filebeat_config)
-    _run('sudo mv {0} /etc/filebeat/filebeat.yml'.format(filebeat_config_file))
-    ctx.logger.info('filebeat.yml was configured...')
+
+    _run('sudo mv {0} {1}'.format(filebeat_config_file, os.path.join('/', 'etc', 'filebeat', 'filebeat.yml')))
+    ctx.logger.info('filebeat was configured...')
 
 
 def _download_file(url, destination):
     filename = url.split('/')[-1]
-    temp_dir = '/tmp'
+    temp_dir = tempfile.tempdir
     local_filename = os.path.join(temp_dir, filename)
     response = requests.get(url, stream=True)
     with open(local_filename, 'wb') as temp_file:
