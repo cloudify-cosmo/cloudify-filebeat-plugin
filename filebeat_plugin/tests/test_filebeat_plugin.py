75725e3311ccf9a78c4c0e353ded6ab4a216b602
fogelomer <omer@gigaspaces.com>
2016-07-19 09:15:30 +0300

    poc+tests

--- filebeat_plugin/tests/test_filebeat_plugin.py
+++ filebeat_plugin/tests/test_filebeat_plugin.py
@@ -66,7 +66,7 @@ class TestFilebeatPlugin(unittest.TestCase):
 
     @patch('tasks.FILEBEAT_CONFIG_FILE_DEFAULT', CONFIG_FILE)
     @patch('tasks.FILEBEAT_INSTALL_PATH_DEFAULT', TEMP_FILEBEAT)
-    @patch(tasks.ctx, mock_install_ctx())
+    @patch('../tasks.ctx', mock_install_ctx())
     def test_configure_with_inputs_no_file(self):
         '''validate configuration was rendered correctly
         and placed on the right place - with file comprison'''
